eabda2d45d6209e71c1b16f12ef7943daf432c32
isaiahgrey93 <isaiahgrey@gmail.com>
2016-05-03 16:42:23 -0700

    switch to double quotes for content code

--- src/api/files/spero.js
+++ src/api/files/spero.js
@@ -1,35 +1,35 @@
 import { experience } from 'isaiahgrey'
 
-const SET_JOB_TITLE = 'SET_JOB_TITLE'
-const SET_COMPANY = 'SET_COMPANY'
-const SET_DESC = 'SET_DESC'
-const SET_TIME = 'SET_TIME'
+const SET_JOB_TITLE = "SET_JOB_TITLE"
+const SET_COMPANY = "SET_JOB_TITLE"
+const SET_DESC = "SET_DESC"
+const SET_TIME = "SET_DESC"
 
 function setTime () {
   return {
     type: SET_TIME,
-    time: 'September 2015 - March 2016',
+    time: "September 2015 - March 2016",
   }
 }
 
 function setCompany () {
   return {
     type: SET_COMPANY,
-    time: 'Spero',
+    time: "Spero",
   }
 }
 
 function setJobTitle () {
   return {
     type: SET_JOB_TITLE,
-    time: 'Lead Engineer',
+    time: "Lead Engineer",
   }
 }
 
 function setJobDesc () {
   return {
     type: SET_DESC,
-    time: 'Develop and design a benevolent community platform for fundraising with a cause. Ecommerce meets GoFundMe.',
+    time: "Develop and design a benevolent community platform for fundraising with a cause. Ecommerce meets GoFundMe.",
   }
 }
 
